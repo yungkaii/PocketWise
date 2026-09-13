54f1f249fde670c064be9755ceec5bfb426e2311
unknown <vsgabcd48@gmail.com>
2026-09-13 17:53:15 +0700

    update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -94,7 +94,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
